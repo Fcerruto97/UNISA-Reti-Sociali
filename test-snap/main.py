877247159610753a16d27cebd14e4a9e39bdc1e5
L043N70 <L043N70@users.noreply.github.com>
2021-05-25 09:14:01 +0200

    Fix draw graph

--- test-snap/main.py
+++ test-snap/main.py
@@ -1,28 +1,21 @@
 from snap import snap
 
 
-def draw(Graph):
+def draw(graph, filename):
     labels = {}
-    for NI in Graph.Nodes():
+    for NI in graph.Nodes():
         labels[NI.GetId()] = str(NI.GetId())
-    Graph.DrawGViz(snap.gvlDot, "output.png", " ", labels)
-
-    UGraph = snap.GenRndGnm(snap.TUNGraph, 10, 50)
-    labels = {}
-    for NI in UGraph.Nodes():
-        labels[NI.GetId()] = str(NI.GetId())
-    UGraph.DrawGViz(snap.gvlDot, "output.png", " ", labels)
-
-    Network = snap.GenRndGnm(snap.TNEANet, 10, 50)
-    labels = {}
-    for NI in Network.Nodes():
-        labels[NI.GetId()] = str(NI.GetId())
-    Network.DrawGViz(snap.gvlDot, "output.png", " ", labels)
+    graph.DrawGViz(snap.gvlDot, filename, " ", labels)
 
 
 def main():
-    G = snap.LoadEdgeListStr(snap.TNGraph, "Wiki-Vote.txt", 0, 1)
-    print("Number of Nodes: %d" % G.GetNodes())
+    # input_file = "Wiki-Vote.txt"  # 7115 nodi
+    input_file = "small.txt"  # 474 nodi
+
+    g = snap.LoadEdgeListStr(snap.TNGraph, input_file, 0, 1)
+    print("Number of Nodes: %d" % g.GetNodes())
+
+    draw(g, "output.png")
 
 
 if __name__ == '__main__':
