b6e498f65f38aa4ced24c01c489a44fac61ea118
L043N70 <L043N70@users.noreply.github.com>
2021-05-28 13:07:14 +0200

    Add load_mtx_graph method

--- utils/io.py
+++ utils/io.py
@@ -1,5 +1,16 @@
 from scipy.io import mmread
+from snap import snap
+
+from utils.graph import add_nodes, add_edges
 
 
 def read_mtx(path):
     return mmread(path)
+
+
+def load_mtx_graph(dataset, first_node, last_node):
+    mtx = read_mtx(dataset)
+    g = snap.TUNGraph.New()
+    add_nodes(g, first_node, last_node)
+    add_edges(g, mtx)
+    return g
